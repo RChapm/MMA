1a4608ff36b6e9554282b233b6579b74e0331811
RChapm <55127178+RChapm@users.noreply.github.com>
2019-09-12 13:49:21 -0700

    Update UFChistory.py

--- UFChistory.py
+++ UFChistory.py
@@ -18,45 +18,46 @@ for n in range(172941):
   ul=0
   uws=0
   uls=0
-  
+
   #the next layer of code needs to iterate through every single fight given each fighter ID
-  for row in df.iterrows():
-    if (n == df['f1fid']):
+  for row in df.itertuples():
+    #print(row[9])
+    #index 9 is f1fid
+    if (n == row[9]):
       df['f1fights'] = uf
       df['f1w'] = uw
       df['f1l']=ul
       df['f1ws'] = uws
       df['f1ls'] = uls
       uf +=1
-      if (df['flresult'] == 'win'):
+      if (row[7] == 'win'):
         uw += 1
         uws += 1
         uls = 0
-      else if (df['flresult'] == 'loss'):
+      elif (row[7] == 'loss'):
         ul += 1
         uls += 1
         uws = 0
       else:
         uws = 0
         uls = 0
-    else if (n == df['f2fid']):
+    elif (n == row[10]):
       df['f2fights'] = uf
       df['f2w'] = uw
       df['f2l']=ul
       df['f2ws'] = uws
       df['f2ls'] = uls
       uf +=1
-      if (df['f2result'] == 'win'):
+      if (row[8] == 'win'):
         uw += 1
         uws += 1
         uls = 0
-      else if (df['f2result'] == 'loss'):
+      elif (row[8] == 'loss'):
         ul += 1
         uls += 1
         uws = 0
       else:
         uws = 0
         uls = 0
-        
-df.to_csv('fighthistory.csv')
-        
+
+df.to_csv('fighthistory.csv')     
