5f7c23392fcaf41fab58d11b0f4e232e084f5d38
RChapm <55127178+RChapm@users.noreply.github.com>
2019-09-13 01:13:33 -0700

    Update randomize.py

--- randomize.py
+++ randomize.py
@@ -7,27 +7,27 @@ import random
 filepath = "/Users/Ryan/Desktop/Apps/AFreshStart/historical.csv"
 df = pd.read_csv(filepath, encoding = "ISO-8859-1")
 
-def swap(a,b):
-  c = row[a]
-  row[a] = row[b]
-  row[b] = c
+def swap(rownuma,rownumb,colnamea,colnameb):
+  f = row[rownuma]
+  df.set_value(row[0],colnamea, row[rownumb])
+  df.set_value(row[0],colnameb, f)
 
 for row in df.itertuples():
   flip = random.randint(0, 1)
   if (flip == 1):
-    swap(5,6)
-    swap(7,8)
-    swap(9,10)
-    swap(14,19)
-    swap(15,20)
-    swap(16,21)
-    swap(17,22)
-    swap(18,23)
-    swap(24,30)
-    swap(25,31)
-    swap(26,32)
-    swap(27,33)
-    swap(28,34)
-    swap(29,35)
+    swap(5,6,'f1name','f2name')
+    swap(7,8,'f1result','f2result')
+    swap(9,10,'f1fid','f2fid')
+    swap(14,19,'f1fights','f2fights')
+    swap(15,20,'f1w','f2w')
+    swap(16,21,'f1l','f2l')
+    swap(17,22,'f1fws','f2fws')
+    swap(18,23,'f1ls','f2ls')
+    swap(24,30,'f1SubW','f2SubW')
+    swap(25,31,'f1KOW','f2KOW')
+    swap(26,32,'f1DecW','f2DecW')
+    swap(27,33,'f1SubL','f2SubL')
+    swap(28,34,'f1KOL','f2KOL')
+    swap(29,35,'f1DecL','f2DecL')
 
 df.to_csv('randhist.csv')
